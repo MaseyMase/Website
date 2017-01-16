a3c345e8d6d2082bf1631e7aa54d854288f954eb
Masyousd Fayazi <Masyousd Fayazi>
2017-01-16 05:20:45 +0100

    added postcss, cssvars, nested(emulating sass functionality

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var watch = require('gulp-watch');
 var postcss = require ('gulp-postcss');
 var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested')
 
 gulp.task('default', function() {
 	console.log("hooray")
@@ -15,7 +17,7 @@ gulp.task('html', function() {
 
 gulp.task('style', function() {
 	return gulp.src('./app/css/style.css')
-	.pipe(postcss([autoprefixer]))
+	.pipe(postcss([cssvars, nested, autoprefixer]))
 	.pipe(gulp.dest('./app/temp/style'))
 });
 
